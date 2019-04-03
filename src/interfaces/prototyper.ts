36cb9eb92fde1646c828852f6c8fd5a1aa5b0b06
Pablo Veiga <vcpablo@gmail.com>
2019-04-03 11:57:52 -0300

    fix(input-elements): fix value property in input elements

--- src/interfaces/prototyper.ts
+++ src/interfaces/prototyper.ts
@@ -1,6 +1,7 @@
 import { Feature } from './ast-mappings'
+import { HtmlElement } from '../../../ui-html/src/classes/html-elements';
 
 export interface Prototyper {
-  generate(features: Feature[]): Promise<string[]>
+  generate(features: Feature[]): Promise<HtmlElement[]>
   //generate(widgets: Widgets[], options: Options): Promise<string[]>
 }
