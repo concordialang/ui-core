aeb37e488630b907ba03cc7385ace9f9caf204ea
Willian <w.goncalves91@gmail.com>
2019-04-09 22:21:14 -0300

    Rebase on master

--- src/interfaces/prototyper.ts
+++ src/interfaces/prototyper.ts
@@ -1,7 +1,6 @@
 import { Feature } from './ast-mappings'
-import { HtmlElement } from '../../../ui-html/src/classes/html-elements';
 
 export interface Prototyper {
-  generate(features: Feature[]): Promise<HtmlElement[]>
+  generate(features: Feature[]): Promise<string[]>
   //generate(widgets: Widgets[], options: Options): Promise<string[]>
 }
