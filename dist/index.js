195a1aa1e218be5a39f3ce62b3950bf55c42ec58
Willian <w.goncalves91@gmail.com>
2019-06-05 18:38:03 -0300

    Delete the "createFile" function

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
-"use strict";
+'use strict'
 function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+	for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p]
 }
-Object.defineProperty(exports, "__esModule", { value: true });
-__export(require("./ast-processor"));
-__export(require("./classes"));
+Object.defineProperty(exports, '__esModule', { value: true })
+__export(require('./ast-processor'))
+__export(require('./classes'))
