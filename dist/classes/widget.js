6cf9a71ca83544ab8dd02a1207244439a5ae0f63
Willian Gonçalves <w.goncalves91@gmail.com>
2019-06-06 10:38:42 -0300

    Remove the createFile function
    * 0.2.0
    
    * Delete the "createFile" function

--- dist/classes/widget.js
+++ dist/classes/widget.js
@@ -1,13 +1,13 @@
-'use strict'
-Object.defineProperty(exports, '__esModule', { value: true })
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 /**
  * This class must be used by plugins to generate UI elements.
  */
-var Widget = /** @class */ (function() {
-	function Widget(props, name) {
-		this.props = props
-		this.name = name
-	}
-	return Widget
-})()
-exports.Widget = Widget
+var Widget = /** @class */ (function () {
+    function Widget(props, name) {
+        this.props = props;
+        this.name = name;
+    }
+    return Widget;
+}());
+exports.Widget = Widget;
--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,3 @@
 export * from './ast-processor';
 export * from './interfaces';
 export * from './classes';
-export * from './utils';
