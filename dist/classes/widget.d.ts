19a060aca7561c0b5d69254d368255c6d6765530
Willian <w.goncalves91@gmail.com>
2019-06-06 20:21:58 -0300

    Fix the type of widget name

--- dist/classes/widget.d.ts
+++ dist/classes/widget.d.ts
@@ -2,10 +2,10 @@
  * This class must be used by plugins to generate UI elements.
  */
 export declare abstract class Widget {
-    protected name?: string | undefined;
+    protected name: string;
     protected props: {
         [key: string]: string | number | boolean | any[];
     };
-    constructor(props: any, name?: string);
+    constructor(props: any, name: string);
     abstract renderToString(): string;
 }
--- src/classes/widget.ts
+++ src/classes/widget.ts
@@ -2,10 +2,10 @@
  * This class must be used by plugins to generate UI elements.
  */
 export abstract class Widget {
-  protected name?: string | undefined
+  protected name: string
   protected props: { [key: string]: string | number | boolean | any[] }
 
-  constructor(props: any, name?: string) {
+  constructor(props: any, name: string) {
     this.props = props
     this.name = name
   }
